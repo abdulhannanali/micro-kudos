625c924ee946cb8dcdf66c895bd1efe11ef5882a
Hannan Ali <abdulhannanali@outlook.com>
2017-01-23 18:16:10 +0500

    lint stuff

--- src/redis.js
+++ src/redis.js
@@ -13,12 +13,12 @@ const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 const client = createClient(REDIS_URL)
 
 client.on('ready', (event) => {
-    console.log('Redis client is ready')
+  console.log('Redis client is ready')
 })
 
 client.on('error', (error) => {
-    console.error('error occured while connecting to client')
-    console.error(error)
+  console.error('error occured while connecting to client')
+  console.error(error)
 })
 
 module.exports = client
